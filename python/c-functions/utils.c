760d850ffb02d5e73a4dfc3395cd1800c095bfba
Pax <olydaun@hotmail.fr>
2021-05-28 21:43:55 +0200

    oui

--- python/c-functions/utils.c
+++ python/c-functions/utils.c
@@ -11,15 +11,13 @@ void periodic_conditions(double* delta, double* box, void* out) {
     }
 }
 
-// double* distance(double* p1, double* p2, double* box, bool periodic, double* results) {
-//     double dx = p2[0] - p1[0];
-//     double dy = p2[1] - p1[1];
-//     double dz = p2[2] - p1[2];
-//     double delta[3] = {dx, dy, dz};
+double* distance(double* p1, double* p2, double* box, bool periodic, double* results) {
+    double dx = p2[0] - p1[0];
+    double dy = p2[1] - p1[1];
+    double dz = p2[2] - p1[2];
+    double delta[3] = {dx, dy, dz};
 
-//     if (periodic == true) {
-//         results = periodic_conditions(delta, box);
-//     }
-
-//     return results;
-// }
+    if (periodic == true) {
+        results = periodic_conditions(delta, box);
+    }
+}
