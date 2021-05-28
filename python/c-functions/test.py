f375caa50b3aec28fc659955b47414b1df7c6770
Pax <olydaun@hotmail.fr>
2021-05-28 15:19:57 +0200

    c'est aussilent que python

--- python/c-functions/test.py
+++ python/c-functions/test.py
@@ -1,5 +1,6 @@
 from ctypes import *
 import numpy as np
+import timeit
 so_file = "./utils.so"
 utils = cdll.LoadLibrary(so_file)
 
@@ -9,6 +10,17 @@ for i in range(3):
     box[i] = 3
     distance[i] = i
 
-output = utils.periodic_conditions(c_void_p(distance.ctypes.data),
-                                   c_void_p(box.ctypes.data))
-print(output)
+print(timeit.timeit('utils.periodic_conditions(c_void_p(distance.ctypes.data),'
+                                              'c_void_p(box.ctypes.data),'
+                                              'c_void_p(distance.ctypes.data))',
+                    'from test import utils, distance, box, c_void_p',
+                    number=1000000))
+
+
+def periodic_conditions(d: np.ndarray, box: np.ndarray):
+    for i in range(len(d)):
+        d[i] = d[i] - int(round(d[i] / box[i])) * box[i]
+    return d
+
+
+print(timeit.timeit('periodic_conditions(distance, box)', 'from test import periodic_conditions, distance, box', number=1000000))
--- python/c-functions/utils.c
+++ python/c-functions/utils.c
@@ -4,22 +4,22 @@
 #include "utils.h"
 
 
-double* periodic_conditions(double* delta, double* box) {
+void periodic_conditions(double* delta, double* box, void* out) {
+    double* outdata = (double*) out;
     for(int i; i < 3; i++) {
         delta[i] = delta[i] - round(delta[i] / box[i]) * box[i];
     }
-    return delta;
 }
 
-double* distance(double* p1, double* p2, double* box, bool periodic, double* results) {
-    double dx = p2[0] - p1[0];
-    double dy = p2[1] - p1[1];
-    double dz = p2[2] - p1[2];
-    double delta[3] = {dx, dy, dz};
+// double* distance(double* p1, double* p2, double* box, bool periodic, double* results) {
+//     double dx = p2[0] - p1[0];
+//     double dy = p2[1] - p1[1];
+//     double dz = p2[2] - p1[2];
+//     double delta[3] = {dx, dy, dz};
 
-    if (periodic == true) {
-        results = periodic_conditions(delta, box);
-    }
+//     if (periodic == true) {
+//         results = periodic_conditions(delta, box);
+//     }
 
-    return results;
-}
+//     return results;
+// }
