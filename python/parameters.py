c36d68cb7fe7ed2288968882c0cbce8f66a52d6e
Pax <olydaun@hotmail.fr>
2021-06-02 13:22:45 +0200

    box conditions

--- python/parameters.py
+++ python/parameters.py
@@ -5,7 +5,7 @@ This file contains the different constants and parameters to be used
 
 periodic = True
 filename = "test_clathrate/out"
-box_file = "box_size"
+box_file = "box_size_2"
 trimmed_data = 'trimmed_data'
 nframes = 4001
 aop_radius = 0.55
